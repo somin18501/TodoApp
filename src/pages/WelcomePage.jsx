416e437c05bac8da88887c57a510dba81ec1dc18
Poojan Patel <poojanpatel02112002@gmail.com>
2023-06-05 11:35:51 +0530

    Bug fixes

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -49,7 +49,7 @@ export default function WelcomePage(){
             <div className="bg-indigo-300 w-3/4">
                 <div className="h-screen flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="blue" className="bg-indigo-400 w-80 h-80 rounded-full">
-                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
                     </svg>
                 </div>
             </div>
